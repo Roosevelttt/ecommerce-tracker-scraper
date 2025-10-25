00bab26833baf237a691730e58ab069796cafc6d
Roosevelttt <roosevelt1855@gmail.com>
2025-10-25 10:11:41 +0700

    Initial commit

---
+++ test/ecommerce-tracker-scraper.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcommerceTrackerScraper from '../lib/ecommerce-tracker-scraper-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecommerce-tracker-scraper-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcommerceTrackerScraper.EcommerceTrackerScraperStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
